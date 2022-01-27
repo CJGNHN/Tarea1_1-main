287409760a14cbafef9fd3a5399c0447b23afb79
CJGNHN <92118877+CJGNHN@users.noreply.github.com>
2022-01-26 19:17:11 -0600

    Initial commit

---
+++ Tarea1_1/Tarea1_1/MainPage.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Tarea1_1
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        async private void btnSuma_Clicked(object sender, EventArgs e)
+        {
+            //Hacemos la operacion y mandamos el resultado
+
+            int num1 = Convert.ToInt32(n1.Text);
+            int num2 = Convert.ToInt32(n2.Text);
+            int suma = num1 + num2;
+            string totalsuma = suma.ToString();
+
+            //Aqui envio parametros
+             await Navigation.PushAsync(new PageMain(totalsuma));
+        }
+
+        async private void btnResta_Clicked(object sender, EventArgs e)
+        {
+            int num1 = Convert.ToInt32(n1.Text);
+            int num2 = Convert.ToInt32(n2.Text);
+            int resta = num1 - num2;
+            string totalresta = resta.ToString();
+            await Navigation.PushAsync(new PageMain(totalresta));
+        }
+             async private void btnDivision_Clicked(object sender, EventArgs e)
+        {
+            int num1 = Convert.ToInt32(n1.Text);
+            int num2 = Convert.ToInt32(n2.Text);
+            float div = num1 / num2;
+            string totaldiv = div.ToString();
+
+            await Navigation.PushAsync(new PageMain(totaldiv));
+        }
+        async private void btnMultiplicacion_Clicked(object sender, EventArgs e)
+        {
+            int num1 = Convert.ToInt32(n1.Text);
+            int num2 = Convert.ToInt32(n2.Text);
+            int multi = num1 * num2;
+            string totalmulti = multi.ToString();
+
+            await Navigation.PushAsync(new PageMain(totalmulti));
+        }
+
+
+    }
+}
---
+++ Tarea1_1/Tarea1_1/PageMain.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Tarea1_1
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class PageMain : ContentPage
+    {
+        //Aqui recibo datos
+        public PageMain(String total)
+        {
+            InitializeComponent();
+
+            //Aqui concatenamos el texto de nuestro label con el valor del resultado
+            lbltotal.Text = "El resultado es: " + total.ToString() ;
+
+        }
+    }
+}
